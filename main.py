47714433fc5fe27a875b4ccacc6fa5a6f8bc0364
myl1fe <snake220v@gmail.com>
2025-09-04 14:32:08 +0300

    added check for multiple monitors

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 import mss
+import mss.tools
 from datetime import datetime
 import os
+import time
 
 def take_screenshots_mss(save_dir = 'place_sreen'):
     try:
         os.makedirs(save_dir, exist_ok = True)
-        timestap = datetime.now().strftime("%d$m$Y_%H%M%S")
-        filename = os.path.join(save_dir, f'screenshot_{timestap}.png')
+        timestap = datetime.now().strftime("%d%m%Y_%H%M%S")
+        saved_screen = []
 
+        
 
         with mss.mss() as sct:
-            filename = sct.shot(output = filename)
+            count_monitors = sct.monitors
+        
+            if len(count_monitors) > 2:
+                
+                
+                for num, monitors in enumerate(count_monitors[1:], start = 1):
+                    filename = os.path.join(save_dir, f'screenshot_{timestap}_monitor_{num}.png')
+
+                    sct_img = sct.grab(monitors)
+                    mss.tools.to_png(sct_img.rgb, sct_img.size, output=filename)
+                    saved_screen.append(filename)
+
+
+
+            else:
+                filename = os.path.join(save_dir, f'screenshot_{timestap}.png')
+                sct_img = sct.grab(count_monitors[1])
+                mss.tools.to_png(sct_img.rgb, sct_img.size, output=filename)
+                saved_screen.append(filename)
 
         print('uspeshno')
-        return filename
+        return saved_screen
+   
+   
     except Exception as e:
         print(f'ошибика {e}')
         return None
 
 if __name__ == '__main__':
-    take_screenshots_mss()
+    result = take_screenshots_mss()
+    if result:
+        print(f"Созданные файлы: {result}")
